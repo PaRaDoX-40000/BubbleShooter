07024512482aa98af8239f090c8045527aa2358e
PaRaDoX-40000 <43742532+PaRaDoX-40000@users.noreply.github.com>
2022-10-26 14:41:32 +0300

    add bubbles

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField] private int _colorNumber;
+
+    public int ColorNumber => _colorNumber;
+}
